ac330bb7e0030bc16f28ac04737849aa5974d0e4
premchand505 <prem595pop@gmail.com>
2025-06-14 08:17:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>fitness-edge</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          subscribe**
+        </p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
